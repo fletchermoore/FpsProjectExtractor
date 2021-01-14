6652e39286ae6ff12e81162d4e4dd2a955347987
Fletcher Moore <fletcherbmoore@gmail.com>
2021-01-13 19:54:55 -0500

    works

--- FpsProjectExtractor/MainWindow.xaml.cs
+++ FpsProjectExtractor/MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.IO;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -30,11 +31,10 @@ namespace FpsProjectExtractor
         private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
         private static readonly string OutDir = @"C:\Users\fletcher\projects\FpsProjectExtractor\out";
         private static readonly string InDir = @"C:\Users\fletcher\projects\FpsProjectExtractor\in";
-        private static readonly int x = 1049;
-        private static readonly int y = 32;
-        private static readonly int w = 139;
-        private static readonly int h = 31;
-        private static readonly string testImagePath = @"bigtest.jpg";
+        private static readonly int x = 151;
+        private static readonly int y = 61;
+        private static readonly int w = 178;
+        private static readonly int h = 29;
 
         public MainWindow()
         {
@@ -47,6 +47,12 @@ namespace FpsProjectExtractor
             log.Info(msg);
         }
 
+        private string[] InputFiles()
+        {
+            string[] files = Directory.GetFiles(System.IO.Path.Join(InDir, "demo"), "out*");
+            return files;
+        }
+
         private Mat Crop(Mat inMat)
         {
             Mat outMat = new Mat();
@@ -57,29 +63,29 @@ namespace FpsProjectExtractor
         private Mat BlackWhite(Mat inMat)
         {
             Mat outMat = new Mat();
-            CvInvoke.Threshold(inMat, outMat, 0, 100, Emgu.CV.CvEnum.ThresholdType.Binary);
+            CvInvoke.Threshold(inMat, outMat, 200, 255, Emgu.CV.CvEnum.ThresholdType.Binary);
             return outMat;
         }
 
 
-        public void Preprocess(string file)
+        private byte[] Preprocess(string fullPath)
         {
-            string fullPath = System.IO.Path.Join(InDir, file);
             Mat img = CvInvoke.Imread(fullPath, Emgu.CV.CvEnum.ImreadModes.Grayscale);
             System.Drawing.Rectangle roi = new System.Drawing.Rectangle(x, y, w, h);
             Mat croppedImg = new Mat(img, roi);
-            Mat postInverted = new Mat();
-            CvInvoke.BitwiseNot(croppedImg, postInverted);
-            Mat outImg = BlackWhite(postInverted);
+            Mat outImg = new Mat();
+            CvInvoke.BitwiseNot(croppedImg, outImg);
+            //Mat outImg = BlackWhite(postInverted);
             
             VectorOfByte tiff = new Emgu.CV.Util.VectorOfByte();
             CvInvoke.Imencode("*.tiff", outImg, tiff);
+            return tiff.ToArray();
             //string text = Tess(tiff.ToArray());
             //Log(text);
-            outImg.Save(System.IO.Path.Join(OutDir, "6" + file));
+            //outImg.Save(System.IO.Path.Join(OutDir, "7" + file));
         }
 
-        public string? Tess(byte[] data) //string filePath)
+        public string Tess(byte[] data) //string filePath)
         {
             try
             {
@@ -139,13 +145,81 @@ namespace FpsProjectExtractor
             }
         }
 
+        private string CreateLine(string filename, string result)
+        {
+            return $"{filename},{result}";
+        }
+
+        private string Parse(string result)
+        {
+            // Define a regular expression for repeated words.
+            Regex rx = new Regex(@"\b\d+\b",
+              RegexOptions.Compiled | RegexOptions.IgnoreCase);
+
+            // Find matches.
+            MatchCollection matches = rx.Matches(result);
+            if (matches.Count == 0)
+            {
+                return "ERROR";
+            }
+            else
+            {
+                string parsed = "";
+                foreach(Match match in matches)
+                {
+                    parsed += match.Value + ",";
+                }
+                return parsed;
+            }
+        }
+
+        private void WriteResults(List<string> fileNames, List<string> results)
+        {
+            List<string> lines = new List<string>(fileNames.Count);
+            for(int i = 0; i < results.Count; i++)
+            {
+                string parsed = Parse(results[i]);
+                lines.Add(CreateLine(fileNames[i], parsed));
+            }
+            System.IO.File.WriteAllLines(System.IO.Path.Join(OutDir, "out.csv"), lines);
+        }
+
+        private string Process(string file)
+        {
+            byte[] imgData = Preprocess(file);
+            return Tess(imgData);
+        }
+
+        private void DoSubset(int offset = 0, int? limit = null)
+        {
+            string[] files = InputFiles();
+            int count = limit ?? (files.Length - offset);
+            int end = offset + count;
+
+            
+            List<string> fileNames = new List<string>(count);
+            List<string> ocrResults = new List<string>(count);
+
+            for (int i = offset; i < end; i++)
+            {
+                fileNames.Add(files[i]);
+                ocrResults.Add(Process(files[i]));
+            }
+            WriteResults(fileNames, ocrResults);
+        }
+
         public void DoIt(object sender, RoutedEventArgs eventAargs)
         {
             RunBtn.IsEnabled = false;
-            
-            Preprocess(testImagePath);
-            //Tess(testImagePath);
+            Log("Execution start");
+            DoSubset();
             RunBtn.IsEnabled = true;
+            Log("Execution finished");
+        }
+
+        private void Test(object sender, RoutedEventArgs e)
+        {
+            DoSubset(31000,10);
         }
     }
     
